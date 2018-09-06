ebec1e535db212a53f05b0a28271a82b6a9a1cb8
Joshua Behrens <joshua.behrens@niemand.online>
2018-09-06 23:37:55 +0200

    Added diff action for backend

--- Controllers/Backend/ViewSnapshots.php
+++ Controllers/Backend/ViewSnapshots.php
@@ -1,5 +1,8 @@
 <?php
 
+use FroshViewSnapshots\Services\Diff;
+use Shopware\Components\Api\Exception\ParameterMissingException;
+
 /**
  * Class Shopware_Controllers_Backend_ViewSnapshots
  */
@@ -54,5 +57,69 @@ class Shopware_Controllers_Backend_ViewSnapshots extends Shopware_Controllers_Ba
         $this->View()->assign(
             ['success' => true, 'data' => $data, 'total' => $total]
         );
+    }
+
+    /**
+     * @throws ParameterMissingException
+     * @throws Exception
+     */
+    public function diffAction()
+    {
+        $sessionFrom = $this->Request()->getParam('sessionFrom');
+        $stepFrom = $this->Request()->getParam('stepFrom');
+        $sessionTo = $this->Request()->getParam('sessionTo', $sessionFrom);
+        $stepTo = $this->Request()->getParam('stepTo');
+
+        if (empty($sessionFrom)) {
+            throw new ParameterMissingException('sessionFrom is missing');
+        }
+
+        if (empty($stepFrom)) {
+            throw new ParameterMissingException('stepFrom is missing');
+        }
+
+        if (empty($sessionTo)) {
+            throw new ParameterMissingException('sessionTo is missing');
+        }
+
+        if (empty($stepTo)) {
+            throw new ParameterMissingException('stepTo is missing');
+        }
+
+        /** @var Diff $differ */
+        $differ = $this->get('frosh_view_snapshots.services.diff');
+        $dataFrom = $this->getSnapshotStep($sessionFrom, $stepFrom);
+        $dataTo = $this->getSnapshotStep($sessionTo, $stepTo);
+
+        $this->View()->assign([
+            'success' => true,
+            'data' => [
+                'sessionID' => $differ->diffPlain($dataFrom['sessionID'], $dataTo['sessionID'])->renderDiffToHTML(),
+                'template' => $differ->diffPlain($dataFrom['template'], $dataTo['template'])->renderDiffToHTML(),
+                'step' => $differ->diffPlain($dataFrom['step'], $dataTo['step'])->renderDiffToHTML(),
+                'variables' => $differ->diffSerialized($dataFrom['variables'], $dataTo['variables'])->renderDiffToHTML(),
+                'params' => $differ->diffJson($dataFrom['params'], $dataTo['params'])->renderDiffToHTML(),
+            ],
+        ]);
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function getSnapshotStep(string $sessionFrom, int $stepFrom): array
+    {
+        $qb = $this->getModelManager()->getDBALQueryBuilder();
+
+        $qb->select(['*'])
+            ->from('view_snapshots')
+            ->where(
+                $qb->expr()->eq('sessionID', ':sessionId'),
+                $qb->expr()->eq('step', ':step')
+            )
+            ->setParameter('sessionId', $sessionFrom)
+            ->setParameter('step', $stepFrom)
+        ;
+
+        return $qb->execute()->fetch(PDO::FETCH_ASSOC);
     }
 }
